56e42e33f81ed398e9e4ec01b3bd0b4865bce21e
Surai5a <vadym.prymary@gmail.com>
2021-11-01 16:55:25 +0300

    Squashed commit of the following:
    commit 4c708721c33143224d8402686e6261aaa482cc83
    Author: Surai5a <vadym.prymary@gmail.com>
    Date:   Mon Nov 1 16:55:08 2021 +0300
    
        was edited workspace.xml
    
    commit 816b24126535a97688bacf75b1cf45db8ba721c8
    Author: Surai5a <vadym.prymary@gmail.com>
    Date:   Mon Nov 1 16:53:54 2021 +0300
    
        Squashed commit of the following:
    
        commit ca774ebb3365cbb7e0d633a9a28b4b9085ae7016
        Author: Surai5a <vadym.prymary@gmail.com>
        Date:   Mon Nov 1 16:50:58 2021 +0300
    
            fixed individual2
    
    commit 86c9d55fbd64c2e0ad04ea6ae9a99f3b869e4f92
    Author: Surai5a <vadym.prymary@gmail.com>
    Date:   Sun Oct 31 22:25:05 2021 +0300
    
        added all modules and ind tasks
    
    commit 3ded0801836ac5b8bec132efa8cde6486159e0c7
    Author: Surai5a <vadym.prymary@gmail.com>
    Date:   Sun Oct 31 13:36:37 2021 +0300
    
        Added pyProj

---
+++ pyProj/individual2.py
@@ -0,0 +1,53 @@
+import math
+import sys
+
+
+def biqual(a: float,
+           b: float,
+           c: float):
+    if a == 0:
+        print("Wrong number!", file=sys.stderr)
+        exit(0)
+
+    d = math.pow(b, 2) - (4 * a * c)
+    if d < 0:
+        print("No roots")
+        exit(0)
+    elif d > 0:
+        y1 = (-b + math.sqrt(d)) / (2 * a)
+        y2 = (-b - math.sqrt(d)) / (2 * a)
+        if y1 >= 0:
+            x1 = math.sqrt(y1)
+            x2 = -(math.sqrt(y1))
+        elif y2 >= 0:
+            x3 = math.sqrt(y2)
+            x4 = -(math.sqrt(y2))
+        else:
+            print("no roots")
+            exit(0)
+        if y1 >= 0 and y2 >= 0:
+            x1 = math.sqrt(y1)
+            x2 = -(math.sqrt(y1))
+            x3 = math.sqrt(y2)
+            x4 = -(math.sqrt(y2))
+            print(f"x1 = {x1}\nx2 = {x2}\nx3 = {x3}\nx4 = {x4}")
+        elif y1 >=0:
+            print(f"x1 = {x1}\nx2 = {x2}")
+        elif y2 >= 0:
+            print(f"x3 = {x3}\nx4 = {x4}")
+    elif d == 0:
+        y = (-b) / (2 * a)
+        if y > 0:
+            x1 = math.sqrt(y)
+            x2 = -(math.sqrt(y))
+            print(f"x1 = {x1}\nx2 = {x2}")
+        else:
+            print("No roots")
+        exit(0)
+
+
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+
+biqual(a, b, c)
