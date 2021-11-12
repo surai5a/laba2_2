f3727729076b13f7d1e38350ac267610fbed4c24
Vadim <vadym.prymary@gmail.com>
2021-11-12 16:37:56 +0300

    Squashed commit of the following:
    commit a7b83f9492c0be68d3fd2243935d10c43a75063e
    Author: Vadim <vadym.prymary@gmail.com>
    Date:   Tue Nov 2 13:32:58 2021 +0300
    
        Added doc folder
    
        added doc folder
    
    commit 56e42e33f81ed398e9e4ec01b3bd0b4865bce21e
    Author: Surai5a <vadym.prymary@gmail.com>
    Date:   Mon Nov 1 16:55:25 2021 +0300
    
        Squashed commit of the following:
    
        commit 4c708721c33143224d8402686e6261aaa482cc83
        Author: Surai5a <vadym.prymary@gmail.com>
        Date:   Mon Nov 1 16:55:08 2021 +0300
    
            was edited workspace.xml
    
        commit 816b24126535a97688bacf75b1cf45db8ba721c8
        Author: Surai5a <vadym.prymary@gmail.com>
        Date:   Mon Nov 1 16:53:54 2021 +0300
    
            Squashed commit of the following:
    
            commit ca774ebb3365cbb7e0d633a9a28b4b9085ae7016
            Author: Surai5a <vadym.prymary@gmail.com>
            Date:   Mon Nov 1 16:50:58 2021 +0300
    
                fixed individual2
    
        commit 86c9d55fbd64c2e0ad04ea6ae9a99f3b869e4f92
        Author: Surai5a <vadym.prymary@gmail.com>
        Date:   Sun Oct 31 22:25:05 2021 +0300
    
            added all modules and ind tasks
    
        commit 3ded0801836ac5b8bec132efa8cde6486159e0c7
        Author: Surai5a <vadym.prymary@gmail.com>
        Date:   Sun Oct 31 13:36:37 2021 +0300
    
            Added pyProj

---
+++ pyProj/indivvidual3.py
@@ -0,0 +1,13 @@
+import math
+import sys
+
+
+def harsh():
+    num = [10, 12, 18, 20, 21, 24, 27, 30, 36, 40, 42, 45, 48, 50, 54,
+           60, 63, 70, 72, 80, 81, 84, 90]
+    for i in num:
+        print(i, end=' ')
+
+
+print('All 2-char numbers of Harsh Numbers: ')
+harsh()
